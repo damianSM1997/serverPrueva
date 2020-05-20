b3eb9eab00c7790dd5141fb547c69971fc8160e7
damianSM1997 <damiansm97@gmail.com>
2020-05-20 13:53:23 -0500

    primer commir

---
+++ server/config/config.js
@@ -0,0 +1,23 @@
+// ============================
+//  Puerto
+// ============================
+process.env.PORT = process.env.PORT || 3000;
+
+
+// ============================
+//  Entorno
+// ============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ============================
+//  Base de datos
+// ============================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://damian:MI0E1zcZXQP20IwK@cluster0-naamb.mongodb.net/cafe';
+}
+process.env.URLDB = urlDB;
