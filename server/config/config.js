bfcbf73dd64201be5adfd8423fa33e274ce5ed20
damianSM1997 <damiansm97@gmail.com>
2020-05-21 01:24:50 -0500

    esta vez espero funcionn las variables de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -18,6 +18,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://damian:MI0E1zcZXQP20IwK@cluster0-naamb.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI1;
 }
 process.env.URLDB = urlDB;
